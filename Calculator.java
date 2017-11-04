f8151ea6a67083849c617e4e72ba274bae6b052a
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2017-11-04 10:45:49 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+/*
+ Класс реализует калькулятор
+ */
+public class Calculator {
+    /*
+        результат вычисления.
+    */
+    private int result;
+    /*
+        Суммируем аргументы.
+        @param params Аргументы суммирования.
+     */
+    public void add (int ... params){
+        for (Integer param : params) {
+            this.result += param;
+        }
+    }
+    /*
+        получить результат.
+         @return результат вычисления.
+     */
+    public int getResult() {
+        return this.result;
+    }
+    /*
+        Очистить результат вычисления.
+     */
+    public  void cleanResult() {
+        this.result = 0;
+    }
+}
